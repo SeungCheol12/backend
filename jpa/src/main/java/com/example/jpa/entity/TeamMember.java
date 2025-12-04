c6bba1703cbc3a624548315e8d7bd1b625c8cd1a
BaekSeungCheol <kyn04241@gmail.com>
2025-12-04 18:13:33 +0900

    12.04 JPA

---
+++ jpa/src/main/java/com/example/jpa/entity/TeamMember.java
@@ -0,0 +1,32 @@
+package com.example.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Builder
+public class TeamMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+}
